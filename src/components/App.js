7a3d2b84187a830178eea1a79619991a16784bfc
SallyMulupi <mulupisally2gmail.com>
2022-07-22 13:58:38 +0300

    add function

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,7 @@
 // create your App component here
 import React, { useEffect, useState } from "react"
+function App() {
+    const [image, setImage] = useState("")
+    const [load, setLoad] = useState(false)
+
+    useEffect(() => {
